a0877d8d6a6e5e7f3467104855f22efa54f6347f
rupeshpatil <rupeshpatil88927@gmail.com>
2026-01-10 23:59:18 +0530

    implement accounts bulk delete feature

--- app/(dashboard)/accounts/columns.jsx
+++ app/(dashboard)/accounts/columns.jsx
@@ -28,25 +28,17 @@ export const columns = [
     enableSorting: false,
   },
   {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
-    accessorKey: "email",
+    accessorKey: "name",
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Email
+          Name
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
     },
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-  },
+  }
 ];
